a5b9708276038f57333e52b15cd70fe54cb1d0c8
Vineet Kumar Doshi <vineetvivid@gmail.com>
2015-09-11 15:08:46 +0000

    added new subdomains

---
+++ All-Challenges/Python/Closures and Decorators/Standardize mobile number using Decorators.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+stack = []
+for i in range(int(raw_input())):
+  s = raw_input()
+  match = re.findall(r'\w?\w?(\d\d\d\d\d\d\d\d\d\d)',s)
+  stack.append(match[0])
+stack.sort()
+for item in stack:
+  print "+91 "+item[0:5]+" "+item[5:] 
